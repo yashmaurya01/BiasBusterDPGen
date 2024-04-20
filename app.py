38e3c4a32507669999fd8e078c1161b847d05ce1
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2024-04-20 05:55:33 -0400

    completed streamlitg

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from code import BiasMeasure, FindSeed, PromptSyntheticGenerator, DPPromptSyntheticGenerator
+import pandas as pd
+
+# Function to read the API key from a hidden file
+@st.cache(allow_output_mutation=True)
+def get_api_key():
+    with open(".secrets", "r") as f:
+        openai_api_key = f.read().strip()
+    return openai_api_key
+
+openai_api_key = get_api_key()
+
+# Streamlit interface
+st.title("Bias-Busters DP-Gen")
+
+with st.sidebar:
+    st.title("Bias Measurement and Counterfactual Generation")
+    st.write("Balancing Data with Privacy and Fairness")
+
+# File uploader
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+    
+    st.write("CSV File Uploaded and Randomized")
+    
+    # Column selection for analysis
+    column_name = st.selectbox("Select the column for bias measurement:", df.columns)
+    
+    category1 = st.text_input("Enter the first category:", "Single")
+    category2 = st.text_input("Enter the second category:", "Married")
+    
+    if st.button("Measure Bias"):
+        bm = BiasMeasure(openai_api_key)
+        regex_queries = bm.make_query(df)
+        st.write("RegEx Queries Constructed")
+        
+        scores_df, measure_df = bm.evaluate_df(df, regex_queries)
+        st.write("Score and Measure DataFrames Generated")
+        st.dataframe(measure_df)
+        
+        fs = FindSeed(column_name)
+        seed_rows, example_counterfactuals = fs.find_seeds(scores_df)
+        st.write("Examples existing in the dataset:")
+        st.dataframe(example_counterfactuals)
+        
+        psg = PromptSyntheticGenerator(openai_api_key)
+        for majority_sample in seed_rows:
+            st.write("Majority Sample:", majority_sample)
+            minority_sample = psg.generate_synthetic(majority_sample)
+            st.write("Minority Sample Generated:", minority_sample)
+            break
+        
+        # st.write("DP Synthetic Data Generation")
+        # dppsg = DPPromptSyntheticGenerator(openai_api_key, df)
+        # new_point = dppsg.generate_new_point()
+        # st.write("New Data Point Generated:", new_point)
+
+st.write("Ready.")
